220788510441450157394dcddcc8084d2a493abc
Rob Hyndman <robjhyndman@gmail.com>
2020-05-04 09:19:25 +1000

    Functions converted to tidyverse

--- functions-v4.R
+++ functions-v4.R
@@ -15,106 +15,59 @@ autoplot.fmforecast <- function(data) {
   print((p1 | p2 | p3) / ( plot_spacer() | p4 | p5))
 }
 
-
-# Function to produce old age dependency ratio
-oadr <- function(x, sim, pension.age=65, historical.pension.age=65, level=80)
-{
-  adjustment<-(historical.pension.age%%1)*colSums(extract.ages(x,as.integer(historical.pension.age)+1,FALSE)$pop$total,na.rm=TRUE)
-  workers <-colSums(extract.ages(x,15:as.integer(historical.pension.age),FALSE)$pop$total,na.rm=TRUE)+adjustment
-  aged <- colSums(extract.ages(x,as.integer(historical.pension.age):max(x$age),TRUE)$pop$total,na.rm=TRUE)-adjustment
-
-  xhistory <- ts(aged/workers,s=x$year[1],f=1)
-
-  ##ts is "time series" s is starting f is frequency
-
-  h <- dim(sim[[1]])[2]
-  N <- dim(sim[[1]])[3]
-  if(length(pension.age)==1)
-    pension.age <- rep(pension.age, h)
-  if(length(pension.age) != h)
-    stop("length of pension.age should be equal to forecast horizon")
-  # Check that pension.age does not decrease
-  if(max(diff(pension.age)) < 0)
-    stop("Pension ages should not decrease")
-  # Check that pension.age does not increase too quickly
-  if(max(diff(pension.age)) > 1)
-    stop("Pension age should not rise more than 1 year at a time")
-
-  simages <- as.numeric(dimnames(sim[[1]])[[1]]) # Ages vector from sim
-  # Set up matrices to store populations from each forecast horizon and each simulation
-  male.workers <- female.workers <- male.aged <- female.aged <- matrix(NA,nrow=h,ncol=N)
-
-  for (i in (1:h))
-  {
-    worker.rows <- (simages >= 15 & simages < as.integer(pension.age[i]))
-    pension.rows <- (simages >= as.integer(pension.age[i]))
-    adjustment.male<-(pension.age[i]%%1)*sim$male[as.integer(pension.age[i]),i,]
-    adjustment.female<-(pension.age[i]%%1)*sim$female[as.integer(pension.age[i]),i,]
-    male.workers[i,] <- colSums(sim$male[worker.rows,i,])+adjustment.male
-    female.workers[i,] <- colSums(sim$female[worker.rows,i,])+adjustment.female
-    male.aged[i,] <- colSums(sim$male[pension.rows,i,])-adjustment.male
-    female.aged[i,] <- colSums(sim$female[pension.rows,i,])-adjustment.female
-
-  }
-
-  oadp.f <- (male.aged+female.aged)/(male.workers+female.workers)
-  oadp.lo <- apply(oadp.f,1,quantile,prob=(0.5-level/200))
-  oadp.hi <- apply(oadp.f,1,quantile,prob=1-(0.5-level/200))
-
-  firstyear <- min(as.numeric(dimnames(sim[[1]])[[2]]))
-  oadp.f <- structure(list(mean=ts(rowMeans(oadp.f),s=firstyear+1),x=xhistory,
-                           upper=ts(oadp.hi,s=firstyear+1),lower=ts(oadp.lo,s=firstyear+1),
-                           level=level),class="forecast") #s=firstyear+1?
-  return(oadp.f)
+# Function to produce old age dependency ratio using tibbles
+# df is data frame with columns Age, Sex, Year, Population
+# pension_age is dataframe with columns Year, Age.
+oadr <- function(df, pension) {
+  pension <- pension %>%
+    select(Year, Age) %>%
+    rename(Pension = Age)
+  df %>%
+    left_join(pension, by="Year") %>%
+    mutate(
+      # Fill historical years with minimum pension age
+      Pension = replace_na(Pension, min(Pension, na.rm=TRUE)),
+      # Divide population into three groups
+      Group = case_when(
+        (Age >= 15 & Age < Pension) ~ "Workers",
+        (Age >= Pension) ~ "Pension",
+        TRUE ~ "Child"
+      )
+    ) %>%
+    group_by(Year, Group) %>%
+    summarise(Population = sum(Population)) %>%
+    ungroup() %>%
+    pivot_wider(Year, names_from=Group, values_from=Population) %>%
+    mutate(OADR = Pension / Workers) %>%
+    select(Year, OADR)
 }
 
-# Function to produce old age dependency ratio using tibbles
-oadr <- function(history, simulated, pension.age=65, historical.pension.age=65, level=80)
-{
-  history <- history %>%
-    mutate(
-      Group = case_when(
-        (Age >= 15 & Age < historical.pension.age) ~ "Workers",
-        (Age >= historical.pension.age) ~ "Pension",
-        TRUE ~ "Child"
-      )
-    ) %>%
-    group_by(Year, Sex, Group) %>%
-    summarise(Population = sum(Population)) %>%
-    ungroup() %>%
-    pivot_wider(c(Year, Sex), names_from=Group, values_from=Population) %>%
-    mutate(OADP = Pension / Workers) %>%
-    select(Year, Sex, OADP)
-
-  # Simulated OADP
-  oadp_sim <- simulated %>%
-    mutate(
-      Population = case_when(
-        (Age == trunc(pension.age) - 1) ~ Population +  (pension.age %% 1) * lead(Population, 1),
-        (Age == trunc(pension.age)) ~ Population - (pension.age %% 1) * Population,
-        TRUE ~ Population
-      )
-    ) %>%
-    mutate(
-      Group = case_when(
-        (Age >= 15 & Age < pension.age) ~ "Workers",
-        (Age >= pension.age) ~ "Pension",
-        TRUE ~ "Child"
-      )
-    ) %>%
-    group_by(Rep, Year, Sex, Group) %>%
-    summarise(Population = sum(Population)) %>%
-    ungroup() %>%
-    pivot_wider(c(Rep, Year, Sex), names_from=Group, values_from=Population) %>%
-    mutate(OADP = Pension / Workers) %>%
-    select(Year, Sex, Rep, OADP)
-  oadp <- oadp_sim %>%
-    group_by(Sex,Year) %>%
+# As above but df now contains Reps.
+oadr_sim <- function(df, pension, level=80) {
+  # Simulated OADR
+  df %>%
+    group_by(Rep) %>%
+    group_modify(~ oadr(.x, pension=pension)) %>%
+    group_by(Year) %>%
     summarise(
-      Lo = quantile(OADP, prob=(0.5-level/200)),
-      Hi = quantile(OADP, prob=1-(0.5-level/200)),
-      OADP = mean(OADP),
+      Lo = quantile(OADR, prob=(0.5-level/200)),
+      Hi = quantile(OADR, prob=1-(0.5-level/200)),
+      OADR = mean(OADR),
     ) %>%
     ungroup()
-  bind_rows(history, oadp) %>% return()
+}
+
+# As above but df now contains Reps and id
+oadr_rolling_sim <- function(df, pension, level=80) {
+  # Simulated OADR
+  df %>%
+    group_by(Rep,id) %>%
+    group_modify(~ oadr(.x, pension=pension)) %>%
+    group_by(Year,id) %>%
+    summarise(
+      Lo = quantile(OADR, prob=(0.5-level/200)),
+      Hi = quantile(OADR, prob=1-(0.5-level/200)),
+      OADR = mean(OADR),
+    ) %>%
+    ungroup()
 }
