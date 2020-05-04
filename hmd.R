be5e5ee8369b7bf5c9d480ae75baef1b20248c4e
Rob Hyndman <robjhyndman@gmail.com>
2020-05-04 22:48:52 +1000

    Removed hmd.R from repo

--- hmd.R
+++
@@ -1,7 +0,0 @@
-# HMD credentials
-username <- "hanlin.shang@gmail.com"
-password <- "hshang85"
-
-# Get population data for 1 Jan 2015
-auspop <- hmd.pop(country = "AUS", username = username, password = password, label = "Australia")
-auspop <- extract.ages(auspop, age = 0:100)
